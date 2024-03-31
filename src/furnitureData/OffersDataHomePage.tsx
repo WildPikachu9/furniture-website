04ad4ca22ae6a6208d0182a594f68a515b81ef24
YarynaHuk <jasmin.huk@gmail.com>
2024-03-31 18:51:25 +0200

    created offer section for Home page

---
+++ src/furnitureData/OffersDataHomePage.tsx
@@ -0,0 +1,37 @@
+export const offersDataHomePage = [
+  {
+    name: "Meble kuchenne",
+    img: "/img/img2.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble lazienka",
+    img: "/img/img1.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble sypialnia",
+    img: "/img/img2.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble podddasze",
+    img: "/img/img2.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble blala",
+    img: "/img/img1.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble kuchenne",
+    img: "/img/img2.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+  {
+    name: "Meble kuchenne",
+    img: "/img/img1.jpg",
+    desc: "lodolor sit amet, consectetur adipisicing elit. Ad erro fuga distinctio delectus tempore incidunt necessitatibus, labore",
+  },
+];
--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -1,10 +1,12 @@
 import { SetStateAction, useState } from "react";
 import "../App.scss";
 import Carousel from "react-bootstrap/Carousel";
-import { Button } from "../components/Button";
+import { ButtonMain } from "../components/ButtonMain";
 import { Link } from "react-router-dom";
+import { CardSlider } from "../components/CardSlider";
+import { offersDataHomePage } from "../furnitureData/OffersDataHomePage";
 
-export const Home = () => {
+export const HomeCarousel = () => {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex: SetStateAction<number>) => {
@@ -12,33 +14,43 @@ export const Home = () => {
   };
 
   return (
-    <div className='home-container'>
-      <Carousel activeIndex={index} onSelect={handleSelect} fade>
-        <Carousel.Item>
-          <div
-            className='d-block w-100 carousel-img'
-            style={{ backgroundImage: "url(/img/img1.jpg)" }}
-          ></div>
-          <Carousel.Caption className='carousel-desc'>
-            <h1 className='carousel-text'>meble na wymiar</h1>
-            <Button>
-              <Link to='/oferta'>czytaj więcej</Link>
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div
-            className='d-block w-100 carousel-img'
-            style={{ backgroundImage: "url(/img/img2.jpg)" }}
-          ></div>
-          <Carousel.Caption className='carousel-desc'>
-            <h1 className='carousel-text'>wystrój wnętrza</h1>
-            <Button>
-              <Link to='/oferta'>czytaj więcej</Link>
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <Carousel activeIndex={index} onSelect={handleSelect} fade>
+      <Carousel.Item>
+        <div
+          className='d-block w-100 carousel-img'
+          style={{ backgroundImage: "url(/img/img1.jpg)" }}
+        ></div>
+        <Carousel.Caption className='carousel-desc'>
+          <h1 className='carousel-text'>meble na wymiar</h1>
+          <ButtonMain>
+            <Link to='/oferta'>czytaj więcej</Link>
+          </ButtonMain>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div
+          className='d-block w-100 carousel-img'
+          style={{ backgroundImage: "url(/img/img2.jpg)" }}
+        ></div>
+        <Carousel.Caption className='carousel-desc'>
+          <h1 className='carousel-text'>wystrój wnętrza</h1>
+          <ButtonMain>
+            <Link to='/oferta'>czytaj więcej</Link>
+          </ButtonMain>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export const Home = () => {
+  return (
+    <div className='home-layout-container'>
+      <HomeCarousel />
+      <section className='offers-component-container'>
+        <h2 className='main-title'>nasza oferta</h2>
+        <CardSlider data={offersDataHomePage} />
+      </section>
     </div>
   );
 };
