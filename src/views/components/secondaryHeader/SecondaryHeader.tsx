0d1cd479f892e5a0d66809adba1906e068c3a0fe
YarynaHuk <jasmin.huk@gmail.com>
2024-04-08 09:25:09 +0200

    created header for subpages

---
+++ src/views/components/secondaryHeader/SecondaryHeader.tsx
@@ -0,0 +1,15 @@
+import "../../../App.scss";
+import headerImg from "../../../img/img3.jpg";
+
+interface SecondaryHeaderProps {
+  children?: string | JSX.Element;
+}
+
+export const SecondaryHeader = ({ children }: SecondaryHeaderProps) => {
+  return (
+    <div className='secondary-header-component'>
+      <h1 className='header-text'>{children}</h1>
+      <img src={headerImg} alt='asrmeble.' className='secondary-header-img' />
+    </div>
+  );
+};
