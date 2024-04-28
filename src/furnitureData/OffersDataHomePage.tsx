d791aa8f409aa94774f9b2214abfab5c2d4509c1
YarynaHuk <jasmin.huk@gmail.com>
2024-04-28 18:45:27 +0200

    added new photos

--- src/furnitureData/OffersDataHomePage.tsx
+++ src/furnitureData/OffersDataHomePage.tsx
@@ -1,9 +1,11 @@
 import "../i18next";
 import { useTranslation } from "react-i18next";
 
-import img1 from "../img/img1.jpg";
-import img2 from "../img/img2.jpg";
-import img3 from "../img/img3.jpg";
+import kitchen from "../img/banner-1.webp";
+import bedroom from "../img/bedroom/szafa-1.webp";
+import bathroom from "../img/bathroom/photo_2024-04-26_10-11-13.webp";
+import attic from "../img/attic/zabudowa-skosow-3.webp";
+import hall from "../img/hall/hall1.webp";
 
 export const OffersDataHomePage = () => {
   const { t } = useTranslation();
@@ -12,35 +14,35 @@ export const OffersDataHomePage = () => {
     {
       name: t("offersDataHomePage.offer1.name"),
       header: t("offersDataHomePage.offer1.header"),
-      img: img1,
+      img: kitchen,
       desc: t("offersDataHomePage.offer1.desc"),
       descMore: t("offersDataHomePage.offer1.descMore"),
     },
     {
       name: t("offersDataHomePage.offer2.name"),
       header: t("offersDataHomePage.offer2.header"),
-      img: img2,
+      img: hall,
       desc: t("offersDataHomePage.offer2.desc"),
       descMore: t("offersDataHomePage.offer2.descMore"),
     },
     {
       name: t("offersDataHomePage.offer3.name"),
       header: t("offersDataHomePage.offer3.header"),
-      img: img3,
+      img: bedroom,
       desc: t("offersDataHomePage.offer3.desc"),
       descMore: t("offersDataHomePage.offer3.descMore"),
     },
     {
       name: t("offersDataHomePage.offer4.name"),
       header: t("offersDataHomePage.offer4.header"),
-      img: img1,
+      img: attic,
       desc: t("offersDataHomePage.offer4.desc"),
       descMore: t("offersDataHomePage.offer4.descMore"),
     },
     {
       name: t("offersDataHomePage.offer5.name"),
       header: t("offersDataHomePage.offer5.header"),
-      img: img2,
+      img: bathroom,
       desc: t("offersDataHomePage.offer5.desc"),
       descMore: t("offersDataHomePage.offer5.descMore"),
     },
