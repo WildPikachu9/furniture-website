4e490a453454d9a276d18813bb6983d89da7c30f
YarynaHuk <jasmin.huk@gmail.com>
2025-01-12 12:56:11 +0100

    fix issue with scroll during change pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,16 +5,20 @@ import { Offers } from "../views/layout/Offers";
 import { Gallery } from "../views/layout/Gallery";
 import { Contact } from "../views/layout/Contact";
 import { PrivacyPolicy } from "../views/layout/PrivacyPolicy";
+import { ScrollToTop } from "./ScrollToTop";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/o-nas' element={<About />} />
-      <Route path='/oferta' element={<Offers />} />
-      <Route path='/galeria' element={<Gallery />} />
-      <Route path='/kontakt' element={<Contact />} />
-      <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/o-nas' element={<About />} />
+        <Route path='/oferta' element={<Offers />} />
+        <Route path='/galeria' element={<Gallery />} />
+        <Route path='/kontakt' element={<Contact />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+      </Routes>
+    </>
   );
 };
