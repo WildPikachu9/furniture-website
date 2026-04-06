19d8520aac6b1aced288411456cddd11c8310d02
YarynaHuk <jasmin.huk@gmail.com>
2026-04-06 12:28:37 +0200

    update package.json

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -16,3 +16,7 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
+declare module "*.css" {
+  const content: Record<string, string>;
+  export default content;
+}
